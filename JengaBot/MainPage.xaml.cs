e444667f353333b4b890d4c1bd6923f52988c1d4
Chase1325 <clstlaurent@wpi.edu>
2017-04-02 13:16:49 -0400

    Add project files.

---
+++ JengaBot/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace JengaBot
+{
+  
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        void SolveClick(object sender, RoutedEventArgs e)
+        {
+        //TODO: execute code to play jenga
+        SolverProgress.Value += 15;
+        }
+
+        void PlayMoveClick(object sender, RoutedEventArgs e)
+        {
+            //TODO: Execute the move
+            SolverProgress.Value -= 5;
+        }
+    }
+}
