556e9b087b14a0b87b933472c96db1fdfcce5e2c
Chase1325 <clstlaurent@wpi.edu>
2017-04-10 20:19:18 -0400

    Updates to GUI Progress

--- JengaBot/MainPage.xaml.cs
+++ JengaBot/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using JengaBot.Models;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -23,18 +25,78 @@ namespace JengaBot
         public MainPage()
         {
             this.InitializeComponent();
+            Blocks[] blockArray = new Blocks[54];
+
+            if (ViewSelect.IsOn)
+            {
+
+                ViewName.Text = "Left View";
+                //Show Left View
+            } else if (!ViewSelect.IsOn)
+            {
+                ViewName.Text = "Right View";
+                //Show Right View
+            }
         }
 
         void SolveClick(object sender, RoutedEventArgs e)
         {
-        //TODO: execute code to play jenga
-        SolverProgress.Value += 15;
+            //TODO: execute code to play jenga
+            StatusUpdate.Text = "Solver Activated";
         }
 
         void PlayMoveClick(object sender, RoutedEventArgs e)
         {
-            //TODO: Execute the move
-            SolverProgress.Value -= 5;
+            //TODO: Execute the move if valid selection
+            StatusUpdate.Text = "User Played a Move";
+           
+        }
+
+        void SwitchView(object sender, RoutedEventArgs e) {
+            if (ViewSelect.IsOn)
+            {
+                StatusUpdate.Text = "Left View Enabled";
+                ViewName.Text = "Left View";
+                //SHOW the left view
+            }
+
+            if (!ViewSelect.IsOn)
+            {
+                StatusUpdate.Text = "Right View Enabled";
+                ViewName.Text = "Right View";
+                //Show the right view
+            }
+        }
+        
+
+        void CurrentStatus_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void leftView()
+        {
+            
+        }
+
+        void rightView()
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void gridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
